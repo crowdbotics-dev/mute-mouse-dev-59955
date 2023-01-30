cf88f760ea0005926633c2237e0a90dafadd24eb
Crowdbotics <engineering@crowdbotics.com>
2023-01-30 08:22:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mute-mouse-dev-59955",
+  url: "https://mute-mouse-dev-59955.botics.co",
+  api: "https://mute-mouse-dev-59955.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
